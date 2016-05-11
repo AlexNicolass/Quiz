533edb690c4f539b950f8817ed6fd923fef469d4
AlexNicolass <maburrounwebo@hotmail.com>
2016-05-11 12:41:34 +0100

    Tema9: Despliegue BBDD en Heroku

--- models/index.js
+++ models/index.js
@@ -4,9 +4,26 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null,
-			{ dialect: "sqlite", 
-			  storage: "quiz.sqlite"});
+//   DATABASE_URL = sqlite:///
+//   DATABASE_STORAGE = quiz.sqlite
+// Usar BBDD Postgres:
+//   DATABASE_URL = postgres://user:passwd@host:port/database
+
+var url, storage;
+
+if (!process.env.DATABASE_URL) { // entorno de desarrollo local con la BBDD sqlite
+	url = "sqlite:///";
+	storage = "quiz.sqlite";
+} else { // entorno de producción en Heroku con la BBDD Postgres
+	url = process.env.DATABASE_URL;
+	storage = process.env.DATABASE_STORAGE || "";
+}
+
+var sequelize = new Sequelize(url,
+		 	      { storage: storage, 
+				omitNull:true 
+			      });
+
 // Importar la definición de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
