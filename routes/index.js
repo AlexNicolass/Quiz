884877c3b22eaa246a30c693628079cda91e1bde
AlexNicolass <maburrounwebo@hotmail.com>
2016-05-16 10:37:13 +0100

    Tema20: Gestión de usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,8 +14,9 @@ router.get('/author', function(req, res, next) {
   res.render('author');
 });
 
-// Autoload de rutas que usen :quizId
+// Autoload de parametros
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('userId', userController.load); // autoload :userId
 
 // Definición de rutas de /quizzes
 router.get('/quizzes.:format?', 			   quizController.index);
@@ -27,5 +30,14 @@ router.delete('/quizzes/:quizId(\\d+)',	   quizController.destroy);
 
 router.get('/quizzes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments', commentController.create);
+
+// Definición de rutas de cuenta
+router.get('/users', 			userController.index);	// listado usuarios
+router.get('/users/:userId(\\d+)', userController.show);	// ver un usuario
+router.get('/users/new', 		userController.new);	// formulario sign un
+router.post('/users', 			userController.create);// registrar usuario
+router.get('/users/:userId(\\d+)/edit', userController.edit);	// editar cuenta
+router.put('/users/:userId(\\d+)', userController.update);	// actualizar cuenta
+router.delete('/users/:userId(\\d+)', userController.destroy);// borrar cuenta
 
 module.exports = router;
